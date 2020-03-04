a34fbfa495f49166a6fd40f009ac303fb265f970
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-03-04 11:12:22 -0600

    Ajax en jQuery y Javascript

--- src/js/home.js
+++ src/js/home.js
@@ -10,7 +10,7 @@ const getUserAll = new Promise(function (todoBien, todoMal) {
   //Llamar a un api
   setTimeout(function(){
     //Luego de 3 segundos
-    todoBien('Okey');
+    todoBien('Okey en 5 segundos');
   }, 5000)
 })
 
@@ -18,17 +18,17 @@ const getUser = new Promise(function (todoBien, todoMal) {
   //Llamar a un api
   setTimeout(function(){
     //Luego de 3 segundos
-    todoBien('SOkey 2');
+    todoBien('Okey en 3 segundos');
   }, 3000)
 })
 
-/* getUser
-  .then(function() {
-    console.log('Todo esta bien en la vida')
-  })
-  .catch(function(message){
-    console.log(message)
-  }) */
+// getUser
+//   .then(function() {
+//     console.log('Todo esta bien en la vida')
+//   })
+//   .catch(function(message){
+//     console.log(message)
+//   })
 //Promise.all  resuelve todas OK y luego THEN
 //Promise.race resuelve y la primera que termine continua THEN
 //Cuando ocurre algo, se lanza el catch y termina
@@ -41,4 +41,53 @@ Promise.race([
 })
 .catch(function(message){
   console.log(message)
+})
+
+//JQUERY PETICION CORRECTA
+//$.ajax('url',{configuracion})
+$.ajax('https://randomuser.me/api/',{
+  method: 'GET',
+  success: function(data){
+    console.log(data)
+  },
+  error: function(error){
+    console.log(error)
+  }
+})
+
+//JQUERY PETICION CON ERROR
+$.ajax('https://randomuser.me/api/asasasa',{
+  method: 'GET',
+  success: function(data){
+    console.log(data)
+  },
+  error: function(error){
+    console.log(error)
+  }
+})
+
+//JS PETICION 
+fetch('https://randomuser.me/api/')
+  .then(function(response){
+    //console.log(response)
+    return response.json()
+  })
+  .then(function(data){
+    console.log('User:', data.results[0].name.first)
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+
+  //JS PETICION CON FALLO
+fetch('https://randomuser.me/api/asasas')
+.then(function(response){
+  //console.log(response)
+  return response.json()
+})
+.then(function(data){
+  console.log('User:', data.results[0].name.first)
+})
+.catch(function(error){
+  console.log(error)
 })
