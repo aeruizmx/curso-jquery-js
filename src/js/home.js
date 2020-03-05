cbaabbf13035a59d2954b5a4f342b5842141f349
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-03-05 09:34:29 -0600

    Formularios

--- src/js/home.js
+++ src/js/home.js
@@ -14,8 +14,19 @@
       $element.setAttribute(attribute, attributes[attribute]);
     }
   }
-
-  $form.addEventListener('submit', (event) => {
+const BASE_API = 'https://yts.mx/api/v2/';
+function featuringTemplate(peli){
+  return (`<div class="featuring">
+              <div class="featuring-image">
+                <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+              </div>
+              <div class="featuring-content">
+                <p class="featuring-title">Pelicula encontrada</p>
+                <p class="featuring-album">${peli.title}</p>
+              </div>
+            </div>`);
+}
+  $form.addEventListener('submit', async (event) => {
     event.preventDefault();
     $home.classList.add('search-active');
     const $loader = document.createElement('img');
@@ -25,6 +36,10 @@
       width: 50
     });
     $featuringContainer.append($loader);
+    const data = new FormData($form);
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`);
+    const HTMLString = featuringTemplate(peli.data.movies[0]);
+    $featuringContainer.innerHTML = HTMLString;
   });
 
   function videoItemTemplate(movie) {
@@ -63,15 +78,15 @@
     });
   }
 
-  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`);
   const $actionContainer = document.querySelector('#action');
   renderMovieList(actionList.data.movies, $actionContainer);
 
-  const horrorList = await getData('https://yts.mx/api/v2/list_movies.json?genre=horror');
+  const horrorList = await getData(`${BASE_API}list_movies.json?genre=horror`);
   const $horrorContainer = document.querySelector('#horror');
   renderMovieList(horrorList.data.movies, $horrorContainer);
 
-  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`);
   const $animationList = document.querySelector('#animation');
   renderMovieList(animationList.data.movies, $animationList);
   
