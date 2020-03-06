5d4a2b49c04fda4e5cb196c95f15770224704b0a
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-03-06 09:18:28 -0600

    Encontrando elementos en la lista

--- src/js/home.js
+++ src/js/home.js
@@ -9,14 +9,14 @@
   const $home = document.getElementById('home');
   const $featuringContainer = document.querySelector('#featuring');
 
-  function setAttributes($element, attributes){
+  function setAttributes($element, attributes) {
     for (const attribute in attributes) {
       $element.setAttribute(attribute, attributes[attribute]);
     }
   }
-const BASE_API = 'https://yts.mx/api/v2/';
-function featuringTemplate(peli){
-  return (`<div class="featuring">
+  const BASE_API = 'https://yts.mx/api/v2/';
+  function featuringTemplate(peli) {
+    return (`<div class="featuring">
               <div class="featuring-image">
                 <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
               </div>
@@ -25,7 +25,7 @@ function featuringTemplate(peli){
                 <p class="featuring-album">${peli.title}</p>
               </div>
             </div>`);
-}
+  }
   $form.addEventListener('submit', async (event) => {
     event.preventDefault();
     $home.classList.add('search-active');
@@ -37,9 +37,9 @@ function featuringTemplate(peli){
     });
     $featuringContainer.append($loader);
     const data = new FormData($form);
-    const { 
-      data : {
-        movies : pelis
+    const {
+      data: {
+        movies: pelis
       }
     } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`);
     const HTMLString = featuringTemplate(pelis[0]);
@@ -71,7 +71,7 @@ function featuringTemplate(peli){
   }
 
   function renderMovieList(list, $container, category) {
-    if($container.children[0]){
+    if ($container.children[0]) {
       $container.children[0].remove();
     }
     list.forEach((movie) => {
@@ -82,37 +82,57 @@ function featuringTemplate(peli){
     });
   }
 
-  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`);
+  const { data: { movies: actionList } } = await getData(`${BASE_API}list_movies.json?genre=action`);
   const $actionContainer = document.querySelector('#action');
-  renderMovieList(actionList.data.movies, $actionContainer, 'action');
+  renderMovieList(actionList, $actionContainer, 'action');
 
-  const horrorList = await getData(`${BASE_API}list_movies.json?genre=horror`);
+  const { data: { movies: horrorList } } = await getData(`${BASE_API}list_movies.json?genre=horror`);
   const $horrorContainer = document.querySelector('#horror');
-  renderMovieList(horrorList.data.movies, $horrorContainer, 'horror');
+  renderMovieList(horrorList, $horrorContainer, 'horror');
 
-  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`);
+  const { data: { movies: animationList } } = await getData(`${BASE_API}list_movies.json?genre=animation`);
   const $animationList = document.querySelector('#animation');
-  renderMovieList(animationList.data.movies, $animationList), 'animation';
-  
-  
-  
+  renderMovieList(animationList, $animationList), 'animation';
+
+
+
   const $modal = document.getElementById('modal');
   const $overlay = document.getElementById('overlay');
   const $hideModal = document.getElementById('hide-modal');
 
-  const modalTitle = $modal.querySelector('h1')
-  const modalImage = $modal.querySelector('img')
-  const modalDescription = $modal.querySelector('p')
+  const $modalTitle = $modal.querySelector('h1')
+  const $modalImage = $modal.querySelector('img')
+  const $modalDescription = $modal.querySelector('p')
 
-  function showModal($element){
+  function findById(list, id){
+    return list.find( movie => movie.id === parseInt(id, 10))
+  }
+
+  function findMovie(id, category) {
+    switch (category) {
+      case 'action':
+        return findById(actionList, id)
+      case 'horror':
+        return findById(horrorList, id)
+      case 'animation':
+        return findById(animationList, id)
+
+    }
+  }
+
+  function showModal($element) {
     $overlay.classList.add('active');
     $modal.style.animation = 'modalIn .8s forwards';
     const id = $element.dataset.id;
     const category = $element.dataset.category;
+    const data = findMovie(id, category);
+    $modalTitle.textContent = data.title; 
+    $modalImage.setAttribute('src', data.medium_cover_image);
+    $modalDescription.textContent = data.description_full;
   }
 
   $hideModal.addEventListener('click', hideModal);
-  function hideModal () {
+  function hideModal() {
     $overlay.classList.remove('active');
     $modal.style.animation = 'modalOut .8s forwards';
   }
